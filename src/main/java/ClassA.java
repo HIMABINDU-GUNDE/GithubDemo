8121ab763fa9be77bd045a47528da75b7f2fbf07
Hima <himabindu.gunde@gmail.com>
2021-02-19 14:31:07 +0530

    firstcommit

---
+++ src/main/java/ClassA.java
@@ -0,0 +1,11 @@
+public class ClassA {
+    public void getData()
+    {
+        System.out.println("git tutorial");
+    }
+    public static void main(String[] args)
+    {
+        ClassA a=new ClassA();
+        a.getData();
+    }
+}
